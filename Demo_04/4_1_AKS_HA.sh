e9b3151a1a599ba8f45adfaf29d864946d456e68
dbamaster <crobles@dbamastery.com>
2020-04-08 18:11:38 -0600

    Added header to demo 04 script

--- Demo_04/4_1_AKS_HA.sh
+++ Demo_04/4_1_AKS_HA.sh
@@ -1,9 +1,16 @@
-# DEMO 6 - Azure Kubernetes services (AKS) - HA
-#   1- Get pods
-#   2- Get public IP of SQL Server service
-#   3- Simulate failure
-#   4- Connect to SQL Server
-#   5- Show Kubernetes dashboard
+# DEMO 4 - SQL Server HA in AKS
+#   1- Connect to Kubernetes cluster in AKS
+#   2- Get namespaces, nodes, pods and more
+#   3- Check PVC and azure disks provisioned on Kubernetes RG
+#   4- Check pod events
+#   5- Check pod logs
+#   6- Get public IP of SQL Server service
+#   7- Connect with Azure Data Studio
+#   8- Connect to SQL Server to create new database
+#   9- Simulate failure
+#   10- Get SQL Server instance properties
+#   11- Explore database objects
+#   12- Show Kubernetes dashboard
 # -----------------------------------------------------------------------------
 
 # 0- Environment variables | demo path
@@ -40,39 +47,39 @@ az_disk=`kubectl describe pvc pvc-data-plex | grep "Volume:" | awk '{print $2}'`
 az disk list --resource-group MC_PASS-Marathon_endurance_westus --query "[?name=='kubernetes-dynamic-$az_disk'].{Name:name, Size:diskSizeGb, DiskState:diskState}" --output table
 # Go to the portal --> All resources --> Look for PVC disk
 
-# 5- Check pod events
+# 4- Check pod events
 MyPod=`kubectl get pods | grep mssql-plex | awk {'print $1'}`
 kubectl describe pods $MyPod
 
-# 6- Check pod logs
+# 5- Check pod logs
 kubectl logs $MyPod -f
 
-# 8- Get public IP of SQL Server service
+# 6- Get public IP of SQL Server service
 kubectl get service mssql-plex-service
 MyService=`kubectl get service mssql-plex-service | grep mssql-plex | awk {'print $4'}`
 
 # --------------------------------------
 # Azure Data Studio step
 # --------------------------------------
-# 11- Connect with Azure Data Studio
+# 7- Connect with Azure Data Studio
 cd ~
 open  /Applications/Azure\ Data\ Studio.app 
 
-# 10- Connect to SQL Server to create new database
+# 8- Connect to SQL Server to create new database
 sqlcmd -S $MyService,1400 -U SA -P $sa_password -Q "set nocount on; select @@servername;"
 sqlcmd -S $MyService,1400 -U SA -P $sa_password -Q "set nocount on; select @@version;"
 sqlcmd -S $MyService,1400 -U SA -P $sa_password -i "4_2_CreateDatabase.sql"
 sqlcmd -S $MyService,1400 -U SA -P $sa_password -Q "set nocount on; select name from sys.databases;"
 
-# 3- Simulate failure
+# 9- Simulate failure
 cd ~/Documents/$resource_group/Demo_04
 ./4_3_SimulateFailure.sh
 
 # --------------------------------------
 # Azure Data Studio step
 # --------------------------------------
-# 7- Get SQL Server instance properties
-# 8- Explore database objects
+# 10- Get SQL Server instance properties
+# 11- Explore database objects
 
-# 5- Show Kubernetes dashboard
+# 12- Show Kubernetes dashboard
 az aks browse --resource-group $resource_group --name $aks_cluster
